ed75c3280eb42ded7979ef3f8ce12e814e5e7b05
Daniel André <daniel.freitas.nascimento@escola.pr.gov.br>
2026-07-08 12:40:39 -0300

    da

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+onst form = Document.getElementById('form')
+const Input = document.getElementsByClassName('input')
+const todoUL = document.getElementById('todo')
 
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+if(todos){
+    todosforEach(todo => addTodo (todo))}
